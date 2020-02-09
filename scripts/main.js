b7ad75ff930ed9c572e238891ef9aaea848d15a8
uszymyszy <60858230+uszymyszy@users.noreply.github.com>
2020-02-09 23:20:27 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    $(document).ready(function () {
+
+        $('.slider').bxSlider({
+            auto: true,
+            mode: 'fade',
+            pager: false,
+            controls: false,
+            sleep: 4000
+        });
+
+    });
+
+})();
