ea164055de9ba18035330cc72f04437c8c9f1963
uszymyszy <60858230+uszymyszy@users.noreply.github.com>
2022-09-02 13:56:16 +0200

    Add files via upload

--- webmidi/script.js
+++ webmidi/script.js
@@ -53,18 +53,44 @@ function gotMIDImessage(messageData)
 	{
 		lastImg=yourImg;
 
-	if (messageData.data[1]==65)
-	{
-		yourImg = document.getElementById('i1');
-	}
-	if (messageData.data[1]==67)
-	{
-		yourImg = document.getElementById('i2');
-	}
-	if (messageData.data[1]==69)
-	{
-		yourImg = document.getElementById('i3');
-	}
+		switch(messageData.data[1]) {
+			case 1: yourImg = document.getElementById('i1'); break;
+			case 2: yourImg = document.getElementById('i2'); break;
+			case 3: yourImg = document.getElementById('i3'); break;
+			case 4: yourImg = document.getElementById('i4'); break;
+			case 5: yourImg = document.getElementById('i5'); break;
+			case 6: yourImg = document.getElementById('i6'); break;
+			case 7: yourImg = document.getElementById('i7'); break;
+			case 8: yourImg = document.getElementById('i8'); break;
+			case 9: yourImg = document.getElementById('i9'); break;
+			case 10: yourImg = document.getElementById('i10'); break;
+			case 11: yourImg = document.getElementById('i11'); break;
+			case 12: yourImg = document.getElementById('i12'); break;
+			case 13: yourImg = document.getElementById('i13'); break;
+			case 14: yourImg = document.getElementById('i14'); break;
+			case 15: yourImg = document.getElementById('i15'); break;
+			case 16: yourImg = document.getElementById('i16'); break;
+			case 17: yourImg = document.getElementById('i17'); break;
+			case 18: yourImg = document.getElementById('i18'); break;
+
+			default:  break;
+				// code block
+		} 
+
+
+
+	// if (messageData.data[1]==65)
+	// {
+	// 	yourImg = document.getElementById('i1');
+	// }
+	// if (messageData.data[1]==67)
+	// {
+	// 	yourImg = document.getElementById('i2');
+	// }
+	// if (messageData.data[1]==69)
+	// {
+	// 	yourImg = document.getElementById('i3');
+	// }
 	
 	if(yourImg && yourImg.style)
 	{
